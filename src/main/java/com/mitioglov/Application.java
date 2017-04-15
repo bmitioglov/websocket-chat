0cb0404a6bc08a2ce9d04ac810543ccf314335ef
Mitioglov Boris <ibmttz13.5@gmail.com>
2017-04-15 22:04:31 +0300

    Spring boot configuration added

--- src/main/java/com/mitioglov/Application.java
+++ src/main/java/com/mitioglov/Application.java
@@ -1,7 +1,10 @@
 package com.mitioglov;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
+        System.out.println("Test");
     }
 }
