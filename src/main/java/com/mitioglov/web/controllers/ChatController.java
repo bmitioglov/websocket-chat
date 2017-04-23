67337b33e082cb4304d8b6aa3fca86e7cfaa5ca6
Boris Mitioglov <ibmttz13.5@gmail.com>
2017-04-23 18:15:13 +0300

    Websocket configuration added

--- src/main/java/com/mitioglov/web/controllers/ChatController.java
+++ src/main/java/com/mitioglov/web/controllers/ChatController.java
@@ -1,8 +1,13 @@
 package com.mitioglov.web.controllers;
 
+import com.mitioglov.model.domain.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
 
 @Controller
 public class ChatController {
@@ -15,5 +20,13 @@ public class ChatController {
     @RequestMapping(value = "/chat", method = RequestMethod.POST)
     public String chatPost() {
         return "chat";
+    }
+
+    @MessageMapping("/chat.message")
+    public ChatMessage filterMessage(@Payload ChatMessage message, Principal principal) {
+
+        message.setUsername(principal.getName());
+
+        return message;
     }
 }
